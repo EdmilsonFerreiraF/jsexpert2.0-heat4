417b6c74cdebcce33252efeda40043b2fe854929
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-04-07 19:25:49 -0300

    🚀 :rocket: [Add] Request - test JSON response

--- src/request.js
+++ src/request.js
@@ -1,3 +1,5 @@
+const https = require('https')
+
 class Request {
     errorTimeout = (reject, urlRequest) => () => reject(new Error(`timeout at [${urlRequest}] :(`))
 
@@ -6,9 +8,17 @@ class Request {
             setTimeout(this.errorTimeout(reject, url), timeout)
         })
     }
-    
-    async get(url) {
 
+    async get(url) {
+        return new Promise((resolve, reject) => {
+            https.get(url, res => {
+                const items = []
+
+                res.on('data', data => items.push(data))
+                    .on("end", () => resolve(JSON.parse(items.join(""))))
+            })
+            .on("error", reject)
+        })
     }
 
     async makeRequest({ url, method, timeout }) {
