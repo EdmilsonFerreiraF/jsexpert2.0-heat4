1185c32a4867facb6d04a9eb939ba51401b76e4e
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-04-07 17:10:43 -0300

    🎉 :tada: initial commit 🎉

---
+++ test/request.test.js
@@ -0,0 +1,21 @@
+const { describe, it, before, afterEach } = require('mocha')
+
+const assert = require('assert')
+const Request = require('../src/request')
+const { createSandbox } = require('sinon')
+
+describe('Request helpers', () => {
+    let sandbox
+    let request
+
+    before(() => {
+        sandbox = createSandbox()
+        request = new Request()
+    })
+
+    afterEach(() => sandbox.restore())
+    
+    it('should test', () => {
+        assert.ok(true)
+    })
+})
